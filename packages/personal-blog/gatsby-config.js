a819d7596f857387575d5be2241baf62784efb1f
Thanh <thanhnguyen@meditekcorp.com.vn>
2020-01-01 22:25:52 +0700

    - add lib

--- packages/personal-blog/gatsby-config.js
+++ packages/personal-blog/gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     siteUrl: `https://google.com`,
   },
   plugins: [
+    `gatsby-plugin-netlify-identity-widget`,
     `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-plugin-styled-components`,
